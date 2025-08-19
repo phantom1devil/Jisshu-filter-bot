c8f2ef271d5377c608886b859aaa71baad920727
phantom1devil <movies6291@gmail.com>
2025-08-19 09:34:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,11 +45,11 @@ LOG_API_CHANNEL = int(environ.get("LOG_API_CHANNEL", "0"))
 LOG_VR_CHANNEL = int(environ.get("LOG_VR_CHANNEL", "0"))
 
 # MongoDB
-DATABASE_URI = environ.get("DATABASE_URI", "mondo db-mongodb+srv://sahilpatil23347:tnCYUZ8d1RCYGHgH@cluster0.chj8dcu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://sahilpatil23347:tnCYUZ8d1RCYGHgH@cluster0.chj8dcu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get("DATABASE_NAME", "phantombot")
 
 # Files index database url
-FILES_DATABASE = environ.get("FILES_DATABASE", "mondo db-mongodb+srv://sahilpatil23347:tnCYUZ8d1RCYGHgH@cluster0.chj8dcu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+FILES_DATABASE = environ.get("FILES_DATABASE", "mongodb+srv://sahilpatil23347:tnCYUZ8d1RCYGHgH@cluster0.chj8dcu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 COLLECTION_NAME = environ.get("COLLECTION_NAME", "jisshu")
 
 # Other Channel's
